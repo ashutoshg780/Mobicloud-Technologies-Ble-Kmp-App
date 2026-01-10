63d68fcfd7777b3bb7833926dd2b7aacff7da4a0
Ashutosh Gupta <https://github.com/ashutoshg780>
2026-01-10 13:07:16 +0530

    Fix iOS: Add BleManager wrapper for IosBleManager

--- shared/src/iosMain/kotlin/com/mobicloudtechnologies/blekmpapp/Platform.ios.kt
+++ shared/src/iosMain/kotlin/com/mobicloudtechnologies/blekmpapp/Platform.ios.kt
@@ -2,6 +2,10 @@ package com.mobicloudtechnologies.blekmpapp
 
 import com.mobicloudtechnologies.blekmpapp.ble.BleManager
 import com.mobicloudtechnologies.blekmpapp.ble.IosBleManager
+import com.mobicloudtechnologies.blekmpapp.models.BleDevice
+import com.mobicloudtechnologies.blekmpapp.models.ConnectionState
+import com.mobicloudtechnologies.blekmpapp.models.DeviceInfo
+import kotlinx.coroutines.flow.StateFlow
 import platform.UIKit.UIDevice
 
 class IOSPlatform: Platform {
@@ -11,4 +15,28 @@ class IOSPlatform: Platform {
 
 actual fun getPlatform(): Platform = IOSPlatform()
 
-actual fun createBleManager(): BleManager = IosBleManager()
+// Wrapper class to adapt IosBleManager to BleManager interface
+private class IosBleManagerWrapper(private val iosManager: IosBleManager) : BleManager {
+    override val scannedDevices: StateFlow<List<BleDevice>>
+        get() = iosManager.scannedDevices
+
+    override val connectionState: StateFlow<ConnectionState>
+        get() = iosManager.connectionState
+
+    override val deviceInfo: StateFlow<DeviceInfo?>
+        get() = iosManager.deviceInfo
+
+    override fun startScan() = iosManager.startScan()
+
+    override fun stopScan() = iosManager.stopScan()
+
+    override suspend fun connect(device: BleDevice) = iosManager.connect(device)
+
+    override fun disconnect() = iosManager.disconnect()
+
+    override fun isBluetoothEnabled(): Boolean = iosManager.isBluetoothEnabled()
+
+    override fun requestEnableBluetooth() = iosManager.requestEnableBluetooth()
+}
+
+actual fun createBleManager(): BleManager = IosBleManagerWrapper(IosBleManager())
